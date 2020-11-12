544967c6fe5e7d8712e5b857b8b76bf6af273c36
Jonathan Hsu <jonhsu.design@gmail.com>
2020-11-11 19:18:52 -0800

    set up successful logout flow

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect }  from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Button } from "antd";
 import TopicsList from "./TopicsList";
 import MainTopic from "./MainTopic";
 
 const Dashboard = () => {
   const [topic, setTopic] = useState();
+  const history = useHistory();
 
   const viewTopic = topic => {
     setTopic(topic)
+  };
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+    history.push("/login");
   };
 
   return (
@@ -24,7 +30,7 @@ const Dashboard = () => {
             <Button type="secondary">Join Topic</Button>
           </div>
 
-          <h4><Link to="/">Logout</Link></h4>
+          <h4 onClick={() => {logOut()}}><Link to="/">Logout</Link></h4>
         </div>
 
         { topic ? <MainTopic topic={topic} /> : <p>Select a topic from the topics list on the left 😀</p> }
